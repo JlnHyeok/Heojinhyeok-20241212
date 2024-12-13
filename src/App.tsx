3cbf7fe3460dd1456d081624ae46b4148c5bc7e8
JinHyeok <gjwlsgur123@gmail.com>
2024-12-14 02:18:50 +0900

    Feat: Tanstack Query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5분
+      gcTime: 1000 * 60 * 5, // 5분
+      refetchOnWindowFocus: false,
+      retry: 0,
+      throwOnError: true,
+    },
+  },
+});
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
