af15fce94ce2132cbbc78eca5accc965b55c2e41
JinHyeok <gjwlsgur123@gmail.com>
2024-12-14 02:32:18 +0900

    Feat: 타임딜 페이지 구현

--- src/pages/deals/timeDeal/index.tsx
+++ src/pages/deals/timeDeal/index.tsx
@@ -1,22 +1,42 @@
-import { Header } from "@/components";
-import { webPath } from "@/router";
 import { useNavigate } from "react-router-dom";
-
-import styles from "./index.module.css";
+import {
+  BrandDealSection,
+  ErrorBoundary,
+  GlobalFallback,
+  Header,
+  LureDealSection,
+  SectionFallback,
+  TimeDealSection,
+} from "@/components";
+import { webPath } from "@/router";
 
 const TimeDeal = () => {
   const navigate = useNavigate();
-
   const handleClickGoToBrandDeal = () => {
     navigate(webPath.brandDeal());
   };
 
   return (
     <div>
-      <Header title="타임딜" isBackButtonVisible={false} />
-      <button className={styles.button} onClick={handleClickGoToBrandDeal}>
-        브랜드딜 바로가기
-      </button>
+      <ErrorBoundary fallback={GlobalFallback()}>
+        {/* HEADER SECTION */}
+        <Header title="타임특가" isBackButtonVisible={false} />
+
+        {/* LURE DEAL SECTION */}
+        <ErrorBoundary fallback={SectionFallback("순삭 특가")}>
+          <LureDealSection />
+        </ErrorBoundary>
+
+        {/* BRAND DEAL SECTION */}
+        <ErrorBoundary fallback={SectionFallback("브랜드 딜")}>
+          <BrandDealSection
+            handleClickGoToBrandDeal={handleClickGoToBrandDeal}
+          />
+        </ErrorBoundary>
+
+        {/* TIME DEAL SECTION */}
+        <TimeDealSection />
+      </ErrorBoundary>
     </div>
   );
 };
